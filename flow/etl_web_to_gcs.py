d05264476d8a9671e6021eb7c2aec5406cd8e229
Alex <alex.fulbert@telecom-paris.fr>
2023-04-07 11:37:18 +0200

    flow

--- flow/etl_web_to_gcs.py
+++ flow/etl_web_to_gcs.py
@@ -3,17 +3,13 @@ import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
 from random import randint
-from prefect.tasks import task_input_hash
-from datetime import timedelta
 
 
-#task(retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 @task(retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read taxi data from web into pandas DataFrame"""
     # if randint(0, 1) > 0:
     #     raise Exception
-
     df = pd.read_csv(dataset_url)
     return df
 
@@ -21,8 +17,8 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 @task(log_prints=True)
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
-    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+    df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
@@ -44,27 +40,22 @@ def write_gcs(path: Path) -> None:
     gcs_block.upload_from_path(from_path=path, to_path=path)
     return
 
+@flow()
+def etl_web_to_gcs(color, month, year):
+            dataset_file = f"{color}_tripdata_{year}-{month:02}"
+            dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+            df = fetch(dataset_url)
+            df_clean = clean(df)
+            path = write_local(df_clean, color, dataset_file)
+            write_gcs(path)
+
 
 @flow()
-def etl_web_to_gcs(year: int, month: int, color: str) -> None:
+def etl_web_to_gcs_parent(color : str = 'yellow', months : list[int] = [11], year : int = 2020) -> None:
     """The main ETL function"""
-    dataset_file = f"{color}_tripdata_{year}-{month:02}"
-    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
-
-    df = fetch(dataset_url)
-    df_clean = clean(df)
-    path = write_local(df_clean, color, dataset_file)
-    write_gcs(path)
-
-
-@flow()
-def etl_parent_flow( months: list[int] = [1, 2], year: int = 2021, color: str = "yellow"):
     for month in months:
-        etl_web_to_gcs(year, month, color)
-
-
+        etl_web_to_gcs(color, month, year)
+        
 if __name__ == "__main__":
-    color = "yellow"
-    months = [1, 2, 3]
-    year = 2021
-    etl_parent_flow(months, year, color)
+    etl_web_to_gcs_parent()
